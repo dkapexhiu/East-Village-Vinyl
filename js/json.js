4e12610a56d8fc62f998ad82b017887d7a674037
Daniel <daniel.kapexhiu@gmail.com>
2017-07-25 10:20:48 +0200

    Add files via upload

---
+++ js/json.js
@@ -0,0 +1,78 @@
+[
+    {
+        "artist": "Linkin Park",
+        "album": "Meteora",
+        "year": "2003",
+        "tracks": "13" ,
+        "quantity": 1
+    },
+    {
+        "artist": "Fall Out Boy",
+        "album": "American Beauty/ American Psycho",
+        "year": "2015",
+        "tracks": "11",
+        "quantity": 3
+    },
+    {
+        "artist": "Fall Out Boy",
+        "album": "Save Rock and Roll",
+        "year": "2013",
+        "tracks": "11",
+        "quantity": 4
+    },
+    {
+        "artist": "Metallica",
+        "album": "Master of Puppets",
+        "year": "1986",
+        "tracks": "8",
+        "quantity": 2
+    },
+
+    {
+        "artist": "Audioslave",
+        "album": "Audioslave",
+        "year": "2002",
+        "tracks": "14",
+        "quantity": 2
+    },
+
+    {
+        "artist": "Audioslave",
+        "album": "Out of Exile",
+        "year": "2005",
+        "tracks": "12",
+        "quantity": 2
+    },
+
+    {
+        "artist": "Fall Out Boy",
+        "album": "Centuries",
+        "year": "2014",
+        "tracks": "1",
+        "quantity": 2
+    },
+
+    {
+        "artist": "Asking Alexandria",
+        "album": "From Death To Destiny",
+        "year": "2013",
+        "tracks": "13",
+        "quantity": 2
+    },
+
+    {
+        "artist": "Foo Fighters",
+        "album": "Greatest Hits",
+        "year": "2009",
+        "tracks": "16",
+        "quantity": 2
+    },
+
+    {
+        "artist": "The Rolling Stones",
+        "album": "A Bigger Band",
+        "year": "2005",
+        "tracks": "16",
+        "quantity": 2
+    }
+]
